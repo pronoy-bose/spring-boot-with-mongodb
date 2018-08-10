e52f4d4515d539807777321ecf58b1d3f49eeda4
Pronoy Bose <pronoy911@gmail.com>
2018-08-10 19:33:53 +0530

    Commented parking status check

--- vehicle_access_UI/src/app/app.component.ts
+++ vehicle_access_UI/src/app/app.component.ts
@@ -15,32 +15,32 @@ export class AppComponent implements OnInit {
   constructor(private plateDetailsService: PlateDetailsService) { }
 
   ngOnInit() {
-    this.checkParkingStatus();
-    this.plateDetailsService.getParkingFullStatus().subscribe(response => {
-      this.carParkingStatus = response.carParking;
-      this.bikeParkingStatus = response.bikeParking;
-      this.carStatusText = this.carParkingStatus ? "Car Parking Full" : "Car Parking Available";
-      this.bikeStatusText = this.bikeParkingStatus ? "Bike Parking Full" : "Bike Parking Available";
-    },
-      error => {
-        console.log(error);
-      });
+    // this.checkParkingStatus();
+    // this.plateDetailsService.getParkingFullStatus().subscribe(response => {
+    //   this.carParkingStatus = response.carParking;
+    //   this.bikeParkingStatus = response.bikeParking;
+    //   this.carStatusText = this.carParkingStatus ? "Car Parking Full" : "Car Parking Available";
+    //   this.bikeStatusText = this.bikeParkingStatus ? "Bike Parking Full" : "Bike Parking Available";
+    // },
+    //   error => {
+    //     console.log(error);
+    //   });
   }
 
-  checkParkingStatus() {
-    let ref = this;
-    setInterval(() => {
-      ref.plateDetailsService.getParkingFullStatus().subscribe(response => {
-        ref.carParkingStatus = response.carParking;
-        ref.bikeParkingStatus = response.bikeParking;
-        ref.carStatusText = ref.carParkingStatus ? "Car Parking Full" : "Car Parking Available";
-        ref.bikeStatusText = ref.bikeParkingStatus ? "Bike Parking Full" : "Bike Parking Available";
-      },
-        error => {
-          console.log(error);
-        });
-    }, 60000);
+  // checkParkingStatus() {
+  //   let ref = this;
+  //   setInterval(() => {
+  //     ref.plateDetailsService.getParkingFullStatus().subscribe(response => {
+  //       ref.carParkingStatus = response.carParking;
+  //       ref.bikeParkingStatus = response.bikeParking;
+  //       ref.carStatusText = ref.carParkingStatus ? "Car Parking Full" : "Car Parking Available";
+  //       ref.bikeStatusText = ref.bikeParkingStatus ? "Bike Parking Full" : "Bike Parking Available";
+  //     },
+  //       error => {
+  //         console.log(error);
+  //       });
+  //   }, 60000);
 
-  }
+  // }
 
 }
